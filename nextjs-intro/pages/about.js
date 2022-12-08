0f5bfa5769a9569869c8cf17f6027f5e06dfd0eb
suzz-in <o_o_su_jin_@naver.com>
2022-12-08 11:32:46 +0900

    routing

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,13 @@
+import NavBar from "../components/NavBar";
+
 export default function Potato() {
-  return "about us";
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
 
-// 함수이름 관계없이 about page로 라우팅 됨.
+// 함수이름 관계없이 about page로 라우팅 됨. 파일이름으로 라우팅 됨.
 // 프레임워크 : 적절한 곳에 코드 넣어줘야하는 것, 기본적인 수정 x, 프레임워크가 내 코드를 호출할 것 (라이브러리와 다른점)
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,3 +1,14 @@
+import NavBar from "../components/NavBar";
+
 export default function Home() {
-  return "hi";
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  );
 }
+
+//index 페이지 가지고 있는것 아니라, / 페이지로 이동
+//jsx, import React 없이 jsx 요소 사용가능
+//nextjs -> 앱에 있는 페이지들 미리 렌더링 됨 (정적으로 생성됨)
