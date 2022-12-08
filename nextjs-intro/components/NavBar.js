9cdbedce0721f7a12ec8c4e462eb9edaa3c54efd
suzz-in <o_o_su_jin_@naver.com>
2022-12-08 11:51:09 +0900

    css사용(css모듈)

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/" legacyBehavior>
+        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a className={`${styles.link} ${router.pathname === "/about" ? styles.active : ""}`}>
+          About
+        </a>
+      </Link>
     </nav>
   );
 }
